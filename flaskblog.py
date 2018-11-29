2294c9a197b5ab7e37254f6c3e8c475e45367b0c
AshwinChandlapur <ashwinchandlapur@gmail.com>
2018-11-29 16:48:50 -0500

    Starting Flask1

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+
+posts = [
+    {
+        "author":"Ashwin",
+        "title":"Blog 1",
+        "content":"First Post",
+        "date_posted":"Today"
+    },
+
+    {
+        "author":"Ashwin Chandlapur",
+        "title":"Blog 1",
+        "content":"First Post By ",
+        "date_posted":"Today Not Tomorrow"
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Don't go easy on that night.</h1>"
+    return render_template('home.html',posts = posts)
 
 @app.route("/about")
 def about():
-    return "<h1> About Page</h1>"
+    return render_template('about.html',title = "About")
 
 if __name__ == "__main__":
     app.run(debug=True)
